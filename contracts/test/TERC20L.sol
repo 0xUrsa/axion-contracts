1b69061cb659415d4263ff4b306dd6dfb07b4edc
david-liu-dev <davidliudev@outlook.com>
2020-09-25 19:20:52 +0300

    Create TERC20L.sol

---
+++ contracts/test/TERC20L.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+// Rinkeby faucet: https://faucet.rinkeby.io/
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+contract TERC20L is ERC20 {
+    using SafeMath for uint256;
+
+    event Deposit(address indexed dst, uint256 wad, uint256 amountOut);
+    event Withdrawal(address indexed src, uint256 wad);
+
+    uint256 public constant LIMIT = 250000000000e18;
+    uint256 public constant RATE = 1e6;
+
+    constructor(string memory name, string memory symbol)
+        public
+        ERC20(name, symbol)
+    {}
+
+    function deposit() external payable {
+        require(totalSupply() < LIMIT, "limit exceeded");
+        uint256 amountOut = msg.value.mul(RATE);
+        require(totalSupply().add(amountOut) < LIMIT, "insufficient reserve");
+        _mint(msg.sender, amountOut);
+        Deposit(msg.sender, msg.value, amountOut);
+    }
+
+    function withdraw(uint256 wad) external {
+        require(balanceOf[msg.sender] >= wad, "insufficient funds");
+        uint256 amountOut = wad.div(RATE);
+        _mint(msg.sender, amountOut);
+        msg.sender.transfer(wad);
+        Withdrawal(msg.sender, wad);
+    }
+}
