15a845cf2bdbca0f12cff259af70e5b8d563021f
david-liu-dev <davidliudev@outlook.com>
2020-11-07 14:35:45 +0100

    Fix nativeswap and staking test

--- test/staking.js
+++ test/staking.js
@@ -7,6 +7,7 @@ chai.use(require("chai-bn")(BN));
 const TERC20 = artifacts.require("TERC20");
 const Staking = artifacts.require("Staking");
 const SubBalancesMock = artifacts.require("SubBalancesMock");
+const AuctionMock = artifacts.require("AuctionMock");
 
 const DAY = 86400;
 
@@ -14,44 +15,39 @@ contract("Staking", ([bank, foreignSwap, subBalances, staker1, staker2]) => {
   let token;
   let staking;
   let subbalances;
+  let auction;
 
   beforeEach(async () => {
-    token = await TERC20.new("2X Token", "2X", web3.utils.toWei("10000"), {
+    token = await TERC20.new("2X Token", "2X", web3.utils.toWei("10000"), bank, {
       from: bank,
     });
 
     subbalances = await SubBalancesMock.new();
+    auction = await AuctionMock.new();
 
     await token.transfer(staker1, web3.utils.toWei("100"), { from: bank });
     await token.transfer(staker2, web3.utils.toWei("100"), { from: bank });
 
     staking = await Staking.new();
-    await staking.init(token.address, subbalances.address, foreignSwap);
+    await staking.init(token.address, auction.address, subbalances.address, foreignSwap, DAY);
   });
 
-  // it("should stake", async () => {
-  //   await token.approve(staking.address, web3.utils.toWei("10"), {
-  //     from: staker1,
-  //   });
+  it("should stake", async () => {
+    await token.approve(staking.address, web3.utils.toWei("10"), {
+      from: staker1,
+    });
 
-  //   await staking.stake(web3.utils.toWei("10"), 100, {
-  //     from: staker1,
-  //   });
+    await staking.stake(web3.utils.toWei("10"), 1, {
+      from: staker1,
+    });
 
-  //   expect(
-  //     await staking.getAccountSessionsLen(staker1)
-  //   ).to.be.a.bignumber.that.equals(new BN("1", 10));
-
-  //   const sessionId = await staking.sessionsOf(staker1, 0);
-
-  //   const sessionData = await staking.sessionDataOf(staker1, sessionId);
-
-  //   expect(sessionData.amount).to.be.a.bignumber.that.equals(
-  //     web3.utils.toWei("10")
-  //   );
-
-  //   expect(sessionData.sessionIsActive).equals(true);
-  // });
+    const sessionId = await staking.sessionsOf(staker1, 0);
+    const sessionData = await staking.sessionDataOf(staker1, sessionId);
+    
+    expect(sessionData.amount).to.be.a.bignumber.that.equals(
+      web3.utils.toWei("10")
+    );
+  });
 
   it("should make payout", async () => {
     await token.approve(staking.address, web3.utils.toWei("10"), {
@@ -78,57 +74,47 @@ contract("Staking", ([bank, foreignSwap, subBalances, staker1, staker2]) => {
       sharesTotalSupply: sharesTotalSupply.toString(),
     });
 
-    // expect(
-    //   await staking.getAccountSessionsLen(staker1)
-    // ).to.be.a.bignumber.that.equals(new BN("1", 10));
+    const sessionId = await staking.sessionsOf(staker1, 0);
+    const sessionData = await staking.sessionDataOf(staker1, sessionId);
 
-    // expect(sessionData.amount).to.be.a.bignumber.that.equals(
-    //   web3.utils.toWei("10")
-    // );
-
-    // expect(sessionData.sessionIsActive).equals(true);
+    expect(sessionData.amount).to.be.a.bignumber.that.equals(
+      web3.utils.toWei("10")
+    );
   });
 
-  // it("should unstake", async () => {
-  //   await token.approve(staking.address, web3.utils.toWei("10"), {
-  //     from: staker1,
-  //   });
+  it("should unstake", async () => {
+    await token.approve(staking.address, web3.utils.toWei("10"), {
+      from: staker1,
+    });
 
-  //   await staking.stake(web3.utils.toWei("10"), 100, {
-  //     from: staker1,
-  //   });
+    await staking.stake(web3.utils.toWei("10"), 100, {
+      from: staker1,
+    });
 
-  //   await token.approve(staking.address, web3.utils.toWei("10"), {
-  //     from: staker2,
-  //   });
+    await token.approve(staking.address, web3.utils.toWei("10"), {
+      from: staker2,
+    });
 
-  //   await staking.stake(web3.utils.toWei("10"), 100, {
-  //     from: staker2,
-  //   });
+    await staking.stake(web3.utils.toWei("10"), 100, {
+      from: staker2,
+    });
 
-  //   await token.transfer(staker1, web3.utils.toWei("100"), { from: bank });
+    await token.transfer(staker1, web3.utils.toWei("100"), { from: bank });
+    
+    // Forward to the end of the staking period
+    await helper.advanceTimeAndBlock(DAY * 100);
+    
+    await staking.makePayout();
 
-  //   await staking.makePayout();
+    const sessionId = await staking.sessionsOf(staker1, 0);
+    await staking.unstake(sessionId, {
+      from: staker1,
+    });
 
-  //   // Change node time and swap
-  //   await helper.advanceTimeAndBlock(DAY * 100);
+    const sessionData = await staking.sessionDataOf(staker1, sessionId);
 
-  //   await staking.unstake(web3.utils.toWei("10"), 100, {
-  //     from: staker1,
-  //   });
-
-  //   expect(
-  //     await staking.getAccountSessionsLen(staker1)
-  //   ).to.be.a.bignumber.that.equals(new BN("1", 10));
-
-  //   const sessionId = await staking.sessionsOf(staker1, 0);
-
-  //   const sessionData = await staking.sessionDataOf(staker1, sessionId);
-
-  //   expect(sessionData.amount).to.be.a.bignumber.that.equals(
-  //     web3.utils.toWei("10")
-  //   );
-
-  //   expect(sessionData.sessionIsActive).equals(true);
-  // });
+    expect(sessionData.amount).to.be.a.bignumber.that.equals(
+      web3.utils.toWei("10")
+    );
+  });
 });
